3ffb1cfdfb50dd7c8414bf9a4063d5ac36404503
原昂大 <takahiro.hara@ietty.co.jp>
2020-03-04 03:11:27 +0900

    [add]メッセージビューに関する変更を追加

--- SwiftMessagesSample/ViewController.swift
+++ SwiftMessagesSample/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import SwiftMessages
 
 class ViewController: UIViewController {
+    
+    // 各メッセージビューの生成
+    let firstView  = MessageView.viewFromNib(layout: .centeredView)
+    let secondView = MessageView.viewFromNib(layout: .centeredView)
+    let thirdView  = MessageView.viewFromNib(layout: .centeredView)
+    let forthView  = MessageView.viewFromNib(layout: .centeredView)
+
+    // 設定用の変数
+    var config = SwiftMessages.Config()
+    
+    // 画像データ
+    let imgTutorial1 = UIImage.init(named: "imgTutorial1")
+    let imgTutorial2 = UIImage.init(named: "imgTutorial2")
+    let imgTutorial3 = UIImage.init(named: "imgTutorial3")
+    let imgTutorial4 = UIImage.init(named: "imgTutorial4")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 見やすいように背景を灰色に変更
+        self.view.backgroundColor = .gray
+        
+        setFirstView()
+        setSecondView()
+        setThirdView()
+        setForthView()
+        configure()
+        
+        // メッセージの表示
+        SwiftMessages.show(config: config, view: firstView)
+        SwiftMessages.show(config: config, view: secondView)
+        SwiftMessages.show(config: config, view: thirdView)
+        SwiftMessages.show(config: config, view: forthView)
     }
 
-
+    // １枚目のメッセージビューの設定
+    func setFirstView() {
+        firstView.configureContent(title: nil, body: "iettyから物件提案をお届けします。\n物件をタップすると、お部屋の詳細を見ることができます。", iconImage: imgTutorial1, iconText: "いらっしゃいませ！", buttonImage: nil, buttonTitle: "つぎへ") { (button) in
+            SwiftMessages.hide()
+        }
+        
+        firstView.iconLabel?.font = UIFont(name: "HiraginoSans-W3", size: 18)
+        firstView.bodyLabel?.font = UIFont(name: "HiraginoSans-W3", size: 13)
+        firstView.bodyLabel?.textAlignment = NSTextAlignment.left
+        firstView.bodyLabel?.textColor = UIColor(red: 129/255, green: 129/255, blue: 129/255, alpha: 1)
+    }
+    
+    // 2枚目のメッセージビューの設定
+    func setSecondView() {
+        secondView.configureContent(title: nil, body: "内見するボタンを押すと、お部屋の内見予約をすることができます。", iconImage: imgTutorial2, iconText: "物件を評価しよう！", buttonImage: nil, buttonTitle: "つぎへ") { (button) in
+            SwiftMessages.hide()
+        }
+        
+        secondView.iconLabel?.font = UIFont(name: "HiraginoSans-W3", size: 18)
+        secondView.bodyLabel?.font = UIFont(name: "HiraginoSans-W3", size: 13)
+        secondView.bodyLabel?.textAlignment = NSTextAlignment.left
+        secondView.bodyLabel?.textColor = UIColor(red: 129/255, green: 129/255, blue: 129/255, alpha: 1)
+    }
+    
+    // 3枚目のメッセージビューの設定
+    func setThirdView() {
+        thirdView.configureContent(title: nil, body: "「お気に入り」ボタンを押すとお部屋について質問できます。", iconImage: imgTutorial3, iconText: "お部屋について質問しよう", buttonImage: nil, buttonTitle: "つぎへ") { (button) in
+            SwiftMessages.hide()
+        }
+        
+        thirdView.iconLabel?.font = UIFont(name: "HiraginoSans-W3", size: 18)
+        thirdView.bodyLabel?.font = UIFont(name: "HiraginoSans-W3", size: 13)
+        thirdView.bodyLabel?.textAlignment = NSTextAlignment.left
+        thirdView.bodyLabel?.textColor = UIColor(red: 129/255, green: 129/255, blue: 129/255, alpha: 1)
+    }
+    
+    // 4枚目のメッセージビューの設定
+    func setForthView() {
+        forthView.configureContent(title: nil, body: "ご要望やご質問に不動産のプロがお答えします。\nお気軽にチャットで話しかけてください！", iconImage: imgTutorial4, iconText: "チャットで相談しよう", buttonImage: nil, buttonTitle: "はじめる") { (button) in
+            SwiftMessages.hide()
+        }
+        
+        forthView.iconLabel?.font = UIFont(name: "HiraginoSans-W3", size: 18)
+        forthView.bodyLabel?.font = UIFont(name: "HiraginoSans-W3", size: 13)
+        forthView.bodyLabel?.textAlignment = NSTextAlignment.left
+        forthView.bodyLabel?.textColor = UIColor(red: 129/255, green: 129/255, blue: 129/255, alpha: 1)
+    }
+    
+    // 全てのメッセージビューに共通する設定
+    func configure() {
+        // メッセージが消えないようにする設定
+        config.duration = .forever
+        // メッセージが縦方向で見て中央に来るようにする設定
+        config.presentationStyle = .center
+    }
 }
 
